37e3b19c026b90d819334bd72e3f4bb71d71eef2
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-04 06:34:12 -0500

    añadiendo otra forma de sumar dígitos de un número

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
 window.addEventListener("load", cargar);
 var btn =document.getElementById("btn");
 var resultado = document.getElementById("resultado");
-var contador = 1;
+
 function cargar() {
-    btn.addEventListener("click", sumaDigitos);
+    btn.addEventListener("click", sumaDosDigitos);
 }
-function sumaDigitos(e) {
+
+function sumaDosDigitos(e) {
     e.preventDefault();
+	
     var input = document.getElementById("nmro").value;
-    var suma = 0;
-    for (var i = 0; i < input.length; i++) {
-            if (input >= 10 && input <= 99) {
-            suma += parseInt(input[i]);
-            resultado.textContent=suma;
-        }
-        else{
-            resultado.textContent="Solo acepto números de dos cifras";
-        }
-    }
-    input.value="";
+	
+	if(input >= 10 && input <= 99){
+		var suma = parseInt(input/10) + input%10;
+		resultado.textContent=suma;
+	}
+	else{
+		resultado.textContent="Solo acepto números de dos dígitos";
+	}
+	
+	/*if(input >= 10 && input <= 99){
+		var suma = parseInt(input[0])+parseInt(input[1]);
+		resultado.textContent=suma;
+	}
+	else{
+		resultado.textContent="Solo acepto números de dos dígitos";
+	}*/
 }
 
